6690e76ce51ef61870fdd6dbaac31e38ab8e5853
CoCode2018 <3221910341@qq.com>
2018-11-22 08:42:16 +0800

    ?

--- C/明解C语言/第3章 分支结构程序/All.c
+++ C/明解C语言/第3章 分支结构程序/All.c
@@ -1,29 +1,26 @@
 /*
-第三章 分支结构程序
-    1.程序通过条件判断的结果选择性地执行某种处理的情况
-    2.根据条件改变程序流程的基本方法
+关系运算符(relational operator)
+    <
+    <=
+    >
+    >=
+嵌套的if-else语句
 
-3-1 if语句(if statement)
-    1.if(表达式){       // 表达式的值为真(非0)时, 执行相应的语句.(所谓的真/假就是: 非0/0)
-        statements
-    }
-    2.因为括号内表达式的值是if语句的关键, 控制着程序流程的走向, 因此称为控制表达式(control expression)
-    3.遇到if语句时: 判断控制表达式的值的真假,
+判断：程序执行时会对控制表达式的值和类型进行检测也就是判断(evaluation)
+    表达式都有值，程序执行时会对表达式的值进行判断
+条件运算符(conditional operator)
+    ？ ：
+逻辑运算符(logical operator)
+    && (AND)
+    || (OR)
+    ~  (NOT)
+    短路求值(short circuit evaluation)
+        ||，一真则真
+        &&，一假则假
+    
+多分支选择结构
+    switch语句: 通过某一单一表达式的值，将程序分为多个分支的时候，可以使用switch语句
+        其中用来表示程序跳转的标识成为标签(label)，标签必须是常量
+        break语句
 
-    if-else(if-else statement)
-    1.if(表达式){
-        statement1
-    }else{
-        statement2
-    }
-
-    2.控制表达式的值不为0的时候, 执行if后的statement
-      控制表达式的值为0的时候, 执行else后的statement
-
-    3.语法结构图
-
-    4.相等运算符(equality operator)
-        ==
-        !=
-    5.关系运算符()
 */
